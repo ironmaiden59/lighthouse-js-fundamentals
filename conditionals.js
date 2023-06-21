2784023641b86ff6450ee980037d79f86f90bd45
ralph <ralphdcouto1987@gmail.com>
2023-06-21 00:06:58 -0400

    Third Commit

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,11 @@
-const raining = false;
-const cold = false;
+var age = 15
 
-if (!raining) {
-  console.log("Leave your umbrella at home!");
-}
+function whichSchool(age) {
+  return (age < 13) ? "Elementary School"
+  :      (age >= 13 && age <= 18 ) ? "Secondary"
+  :      (age > 18) ? "Lighthouse labs"
+  : "None"
+}  
+console.log(whichSchool(20));
+
+  
