193a2aede4fd51ab474fee9d4b3141c9391fb942
ralph <ralphdcouto1987@gmail.com>
2023-06-22 19:52:35 -0400

    Loop

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,14 @@
-var x = 10;
-while (x <= 25) {
-  console.log('Printing out x = ' + x);
-  x = x + 2;
+var x = 1;
+while (x <= 20) {
+  if (x % 3 ===0 && x % 5 ===0) {
+    console.log("JuliaJames");
+  } else if (x % 3 ===0) {
+    console.log("Julia");
+  } else if (x % 5 ===0) {
+    console.log("James");
+  } {
+    console.log(x);
+  }
+  x = x + 1
 }
+
