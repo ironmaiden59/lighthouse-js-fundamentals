d1e56537fec18e74290da6f998cc579da1c29d1c
ralph <ralphdcouto1987@gmail.com>
2023-06-22 17:33:46 -0400

    fourth commit

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,5 @@
-var age = 15
-
-function whichSchool(age) {
-  return (age < 13) ? "Elementary School"
-  :      (age >= 13 && age <= 18 ) ? "Secondary"
-  :      (age > 18) ? "Lighthouse labs"
-  : "None"
-}  
-console.log(whichSchool(20));
-
-  
+var x = 10;
+while (x <= 25) {
+  console.log('Printing out x = ' + x);
+  x = x + 2;
+}
