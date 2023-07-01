229fd8afb6bf8999ee2d10f4546deffc4a13a4c8
ralph <ralphdcouto1987@gmail.com>
2023-07-01 18:32:58 -0400

    parade

---
+++ xmarkstheSpot.js
@@ -0,0 +1,22 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']
+const finalPosition = (moves) => {
+  var position = [0,0]
+  for (var move of moves) {
+    switch(move) {
+      case('north'):
+        position[1]++
+        break;
+      case('south'):
+        position[1]--
+        break;
+      case('east'):
+        position[0]++
+        break;
+      case('west'):
+        position[0]--
+        break;
+    }
+  }
+  return position
+}
+console.log(finalPosition(moves));
