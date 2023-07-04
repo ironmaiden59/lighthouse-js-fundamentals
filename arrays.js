223bb2a86e5163a342dcee2b7d2229610c02ec3c
ralph <ralphdcouto1987@gmail.com>
2023-07-04 13:41:59 -0400

    Practcing Arrays

--- arrays.js
+++ arrays.js
@@ -1,7 +1,9 @@
 var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
 
-// the variable `i` is used to step through each element in the array
-for (var i = 0; i < donuts.length; i++) {
-    donuts[i] += " hole";
-}
-console.log((donuts));
+donuts.forEach(function(donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  console.log(donut);
+});
+
+ 
