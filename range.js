06c3f2dfdec58e3bf4d7f2bdd83c4ddf75d12773
ralph <ralphdcouto1987@gmail.com>
2023-07-05 17:26:48 -0400

    concatenateArrays.js

--- range.js
+++ range.js
@@ -1,12 +1,8 @@
 function range(start, end, step) {
   let array = [];
-  for (let i = start; i <= end; i+= step){
-    array.push(i);
-   
+  for (let i = start; i <= end ; i+= step){
+    array.push[i]
+  
   }
-  return array;
-    
-  };
-  console.log(range(2, 10, 2));
-console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
+  return array
+}
