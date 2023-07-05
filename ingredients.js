2eb3baa08a586dcdf4a2393064c4232ec1d299e7
ralph <ralphdcouto1987@gmail.com>
2023-07-05 14:52:37 -0400

    ingredients.js

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (let x = 0; x < ingredients.length; x++) {
+  
+  console.log(ingredients.reverse(x));
+}
+
